8a2be37edb4c534bd2f7ced6f32dfd84f4c9cb7e
Marco Teran <marco.tulio.teran@gmail.com>
2022-11-26 08:50:09 -0500

    upd ubidtos code

--- files/codes/ubidots/ubidots.ino
+++ files/codes/ubidots/ubidots.ino
@@ -8,9 +8,12 @@
  * Define Instances and Constants
  ****************************************/
 #define DHTPIN 14
+#define LEDPIN 19       // LED
 const char* WIFI_SSID = "marcoteran";
 const char* WIFI_PASS = "qwerty123";
 const char* UBIDOTS_TOKEN = "BBFF-RWFlY7GGLfXunm0MZSXLo2mbmsgiRP";
+const char* DEVICE_LABEL = "30aea46b9d58";
+const char* VARIABLE_LABEL = "buttonvirtual";
 
 // Instancias: configuraciones iniciales
 #define DHTTYPE DHT11
@@ -33,22 +36,37 @@ Ubidots ubidots(UBIDOTS_TOKEN, UBI_HTTP);
 	pinMode(DHTPIN, INPUT);
   dht.begin();
 
+  pinMode(LEDPIN, OUTPUT);
+  digitalWrite(LEDPIN, LOW);
+
 }
 
 void loop() 
 {
-
 	float h = dht.readHumidity();
   float t = dht.readTemperature();
 
-  ubidots.add("Temperatura", t);  // Change for your variable name
-  ubidots.add("Humedad", h);
+  ubidots.add("temperatura", t);  // Change for your variable name
+  ubidots.add("humedad", h);
 
   bool bufferSent = false;
   bufferSent = ubidots.send();  // Will send data to a device label that matches the device Id
   if (bufferSent) {
     // Do something if values were sent properly
     Serial.println("Valores enviados con exito");
+  }
+
+  bool b = ubidots.get(DEVICE_LABEL, VARIABLE_LABEL);
+  if (b != ERROR_VALUE){
+    Serial.print("Valor del boton: ");
+    Serial.println(b);
+    if(b){
+      digitalWrite(LEDPIN, HIGH);
+    }else{
+      digitalWrite(LEDPIN, LOW);
+    }
+  }else{
+    Serial.println("Hubo error leyendo el boton");
   }
 
 	// Printing the results on the serial monitor
